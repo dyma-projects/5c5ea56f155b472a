527daa61ce443bac5e2125dd28269359417e9428
ChuGui <ChuGui>
2019-03-13 16:32:35 +0100

    Initialisation du projet 1

--- projet1/README.md
+++ projet1/README.md
@@ -1,4 +1,4 @@
-# ProjetDyma
+# Projet1
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
 
--- projet1/src/app/app.component.spec.ts
+++ projet1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projet-dyma'`, () => {
+  it(`should have as title 'projet1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('projet-dyma');
+    expect(app.title).toEqual('projet1');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to projet-dyma!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to projet1!');
   });
 });
